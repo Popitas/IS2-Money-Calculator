0dfb0a6a73f3cd523c24592623d31945bf2eede2
usuario <usuario@10.22.143.16>
2014-10-27 14:29:07 +0000

    To-do: CurrencySetLoader()

--- src/view/persistence/CurrencySetLoader.java
+++ src/view/persistence/CurrencySetLoader.java
@@ -1,5 +1,22 @@
 package view.persistence;
 
+import java.util.TreeSet;
+import model.Currency;
+
 public class CurrencySetLoader {
 
+    public void load() {
+        TreeSet<Currency> currencies = new TreeSet<>();
+        
+        fill(currencies);
+    }
+    
+    public TreeSet<Currency> fill(TreeSet<Currency> currencies) {
+        for (int i = 0; i < 100; i++) {
+            //currencies.add(i);
+            
+        }
+        return currencies;
+    }
+    
 }
